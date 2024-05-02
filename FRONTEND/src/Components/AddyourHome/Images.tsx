9ea41739bc8c2bb16389f1bd396902fca7b9cc8e
arjun <arjun.vmaniyara@gmail.com>
2024-05-02 13:44:27 +0530

    image uploading bug

--- FRONTEND/src/Components/AddyourHome/Images.tsx
+++ FRONTEND/src/Components/AddyourHome/Images.tsx
@@ -3,7 +3,6 @@ import { useFormik } from 'formik';
 import Axios from "../Utils/Ssrvice/axios"
 import img4 from '../../assets/contact-img.png'
 
-
 const Images = ({ handleFormDataChange }) => {
   const [selectedImages, setSelectedImages] = useState([]);
   const [error, setError] = useState('');
@@ -13,14 +12,12 @@ const Images = ({ handleFormDataChange }) => {
 
   const isValidImage = (file) => {
     const allowedTypes = ['image/jpeg', 'image/png'];
-   
 
     if (!allowedTypes.includes(file.type)) {
       setError('Please select a valid image file (JPEG or PNG).');
       return false;
     }
 
-  
     return true;
   };
 
@@ -37,11 +34,11 @@ const Images = ({ handleFormDataChange }) => {
         newImages.push(file);
       } else {
         setError('Invalid image format or size.');
+        return;
       }
     }
 
     setSelectedImages([...selectedImages, ...newImages]);
-
   };
 
   const formik = useFormik({
@@ -56,24 +53,12 @@ const Images = ({ handleFormDataChange }) => {
       try {
         setLoading(true);
 
-        const selectedImagesArray = Array.from(selectedImages);
-        
-        const s3Urls = await Promise.all(selectedImagesArray.map(async (file) => {
+        const uploadedImageUrls = [];
+        for (const file of selectedImages) {
           const imageResponse = await Axios.get('/s3service');
+          const s3Url = imageResponse.data.response;
 
-
-
-          return imageResponse.data.response;
-        }));
-
-
-        const uploadPromises = selectedImages.map(async (file, index) => {
-          const s3Url = s3Urls[index];
-
-          console.log(s3Url);
-          
-
-          const imageUploadResponse = await fetch(s3Url, {   
+          const imageUploadResponse = await fetch(s3Url, {
             method: 'PUT',
             body: file,
             headers: {
@@ -81,16 +66,12 @@ const Images = ({ handleFormDataChange }) => {
             },
           });
           const userimage = imageUploadResponse.url.split('?')[0];
-          return userimage;
-        });
-
-        const uploadedImageUrls = await Promise.all(uploadPromises);
-
+          uploadedImageUrls.push(userimage);
+        }
 
         const expirationTime = new Date(Date.now() + 3 * 60 * 1000);
         const uploadedImagesCookieValue = JSON.stringify({ step5Data: uploadedImageUrls });
         document.cookie = `uploadedImages=${uploadedImagesCookieValue}; expires=${expirationTime.toUTCString()}; path=/`;
-
 
         handleFormDataChange({ step5Data: uploadedImageUrls });
         setCookiePreview(uploadedImageUrls);
@@ -102,8 +83,8 @@ const Images = ({ handleFormDataChange }) => {
       }
     },
   });
-  useEffect(() => {
 
+  useEffect(() => {
     const cookieValue = document.cookie.split('; ').find((row) => row.startsWith('uploadedImages='));
 
     if (cookieValue) {
@@ -111,7 +92,6 @@ const Images = ({ handleFormDataChange }) => {
       setCookiePreview(data.step5Data);
     }
   }, []);
-
 
   return (
     <div className="mx-auto max-w-2xl px-4 py-1 sm:px-6 sm:py-12 lg:max-w-7xl lg:px-8 flex flex-col sm:flex-row  mb-30 ">
@@ -134,7 +114,7 @@ const Images = ({ handleFormDataChange }) => {
               <div key={index} className="relative">
 
                 <input
-                 key={index}
+                  key={index}
                   type="file"
                   accept="image/*"
                   onChange={(e) => handleImageChange(e, index)}
@@ -168,14 +148,14 @@ const Images = ({ handleFormDataChange }) => {
                 }`}
               disabled={loading}
             >
-              <div className="flex items-center justify-center"> 
+              <div className="flex items-center justify-center">
                 {loading ? (
                   <>
                     <svg width="20" height="20" fill="currentColor" className="mr-2 animate-spin" viewBox="0 0 1792 1792" xmlns="http://www.w3.org/2000/svg">
                       <path d="M526 1394q0 53-37.5 90.5t-90.5 37.5q-52 0-90-38t-38-90q0-53 37.5-90.5t90.5-37.5 90.5 37.5 37.5 90.5zm498 206q0 53-37.5 90.5t-90.5 37.5-90.5-37.5-37.5-90.5 37.5-90.5 90.5-37.5 90.5 37.5 37.5 90.5zm-704-704q0 53-37.5 90.5t-90.5 37.5-90.5-37.5-37.5-90.5 37.5-90.5 90.5-37.5 90.5 37.5 37.5 90.5zm1202 498q0 52-38 90t-90 38q-53 0-90.5-37.5t-37.5-90.5 37.5-90.5 90.5-37.5 90.5 37.5 37.5 90.5zm-964-996q0 66-47 113t-113 47-113-47-47-113 47-113 113-47 113 47 47 113zm1170 498q0 53-37.5 90.5t-90.5 37.5-90.5-37.5-37.5-90.5 37.5-90.5 90.5-37.5 90.5 37.5 37.5 90.5zm-640-704q0 80-56 136t-136 56-136-56-56-136 56-136 136-56 136 56 56 136zm530 206q0 93-66 158.5t-158 65.5q-93 0-158.5-65.5t-65.5-158.5q0-92 65.5-158t158.5-66q92 0 158 66t66 158z">
                       </path>
                     </svg>
-                    <span>Loading</span> 
+                    <span>Loading</span>
                   </>
                 ) : formSubmitted ? (
                   'Added!'
@@ -184,18 +164,7 @@ const Images = ({ handleFormDataChange }) => {
                 )}
               </div>
             </button>
-
-
-            {/* <button
-              type="submit"
-              className={`mt-3 p-3 w-32  rounded-md ${formSubmitted ? 'bg-green-500 text-white' : 'bg-[#390b79] text-white'
-                }`}
-            >
-              {formSubmitted ? 'Added!' : 'Add'}
-            </button> */}
-
           </div>
-
         </form>
       </div>
     </div>
